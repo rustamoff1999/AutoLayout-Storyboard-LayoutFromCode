4f40115e4c35fef9e825f97dcf3a8fd9a1f02a75
fuadrustamov999 <rustamoff1999@gmail.com>
2020-08-11 21:49:01 +0400

    First experience with Auto Layout and Storyboard

---
+++ Buttons/Buttons/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Buttons
+//
+//  Created by User on 11/08/2020.
+//  Copyright © 2020 Fuad Rustamov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func buttonTapped(_ sender: UIButton) {
+      if sender.title(for: .normal) == "X" {
+        sender.setTitle("A very long title for this button", for: .normal)
+      } else {
+        sender.setTitle("X", for: .normal)
+      }
+    }  
+
+}
+

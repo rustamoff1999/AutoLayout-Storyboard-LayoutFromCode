bdfe23b74882c08a5f5c3372ae8f151340864793
fuadrustamov999 <rustamoff1999@gmail.com>
2020-08-13 00:35:01 +0400

    Application with 4 images

---
+++ Gallery/Gallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gallery
+//
+//  Created by User on 11/08/2020.
+//  Copyright © 2020 Fuad Rustamov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
